78ba02011afedd07a5737a54697420a8fe89def0
forsterlabs <48025445+forsterlabs@users.noreply.github.com>
2021-10-19 08:52:31 -0500

    Adding simple validation for conference tracks prior to output

--- src/main/java/org/forsterlabs/conference_track_management/services/CommandLineService.java
+++ src/main/java/org/forsterlabs/conference_track_management/services/CommandLineService.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,6 +21,8 @@ public class CommandLineService implements CommandLineRunner {
 
     private TrackBuilderService trackBuilderService;
 
+    private boolean validTracks;
+
     public CommandLineService(SessionFileReaderService sessionFileReaderService, TrackBuilderService trackBuilderService) {
         this.sessionFileReaderService = sessionFileReaderService;
         this.trackBuilderService = trackBuilderService;
@@ -30,13 +33,19 @@ public class CommandLineService implements CommandLineRunner {
         boolean userContinue = true;
         String userPrompts = "";
         while (userContinue) {
+            this.validTracks = true;
             Scanner scannerObj = new Scanner(System.in);
             System.out.println("Enter file location of conference sessions:");
             userPrompts = scannerObj.nextLine();
             File sessionFile = new File(userPrompts);
             if (sessionFile.exists()) {
-                outputConferenceTracks(trackBuilderService.buildTracks(sessionFileReaderService
-                        .readConferenceSessionFile(sessionFile)));
+                Map<Integer, List<TrackSession>> tracks = trackBuilderService.buildTracks(sessionFileReaderService
+                        .readConferenceSessionFile(sessionFile));
+                validateTracks(tracks);
+                if (this.validTracks) {
+                    outputConferenceTracks(tracks);
+                }
+
             } else {
                 System.out.println("File not found " + userPrompts);
             }
@@ -63,5 +72,19 @@ public class CommandLineService implements CommandLineRunner {
                 session.getStartTime().getTime()) + " " + session.getSessiongTitle() + " "
                 + (session.getSessionLength() == ConferenceTrackConfig.LIGHTNING_TALK_LENGTH ?
                 ConferenceTrackConfig.LIGHTNING_KEYWORD : session.getSessionLength() + "min");
+    }
+
+    private void validateTracks(Map<Integer, List<TrackSession>> tracks) {
+        tracks.keySet().forEach(integer -> {
+            if (tracks.get(integer).stream().filter(session -> session.getStartTime().get(Calendar.HOUR_OF_DAY) < 12).count() < 2) {
+                System.out.println("Track " + integer + " is missing talks for the morning session");
+                this.validTracks = false;
+            }
+
+            if (tracks.get(integer).stream().filter(session -> session.getStartTime().get(Calendar.HOUR_OF_DAY) >= 13 && session.getStartTime().get(Calendar.HOUR_OF_DAY) < 17).count() < 2) {
+                System.out.println("Track " + integer + " is missing talks for the afternoon session");
+                this.validTracks = false;
+            }
+        });
     }
 }
